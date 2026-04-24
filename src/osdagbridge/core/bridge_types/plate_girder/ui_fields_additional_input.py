a6029d58b8d4eb7dd0f1b35307e493ae6d27c79b
nishikantmandal007 <nishikantmandal007@gmail.com>
2026-04-24 21:00:42 +0530

    feat(schema): expand additional input tab definitions

--- src/osdagbridge/core/bridge_types/plate_girder/ui_fields_additional_input.py
+++ src/osdagbridge/core/bridge_types/plate_girder/ui_fields_additional_input.py
@@ -433,6 +433,7 @@ LANE_DETAILS_TAB_SCHEMA = {
 PERMANENT_LOAD_TAB_SCHEMA = {
     "id": "permanent_load_tab",
     "label_width": 220,
+    "description": {"title": "Description Box", "text": ""},
     "sections": [
         {
             "title": "Dead Load (DL)",
@@ -470,6 +471,7 @@ LIVE_LOAD_TAB_SCHEMA = {
                 "Class 70R Bogie",
             ],
             "bind": "irc_vehicle_checkboxes",
+            "label_bind": "irc_vehicle_labels",
             "default_checked": True,
         },
         {
@@ -478,12 +480,18 @@ LIVE_LOAD_TAB_SCHEMA = {
             "type": "custom_vehicle_table",
             "bind": "custom_vehicle_table",
             "add_button_bind": "custom_vehicle_add_button",
+            "frame_bind": "custom_vehicle_box",
+            "header_label_bind": "custom_vehicle_header_label",
         },
         {
             "id": "braking_section",
             "title": "Braking Load from Vehicles",
             "type": "dynamic_checkbox_list",
             "bind": "braking_vehicle_checkboxes",
+            "layout_bind": "braking_checkboxes_layout",
+            "title_bind": "braking_section_label",
+            "label_bind": "braking_vehicle_labels",
+            "container_bind": "braking_checkboxes_container",
             "default_checked": True,
         },
         {
@@ -709,6 +717,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "As per Code",
                     "bind_mode": "drag_coeff_combo",
                     "bind_value": "drag_coeff_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -744,6 +753,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "Automatic",
                     "bind_mode": "super_area_elev_combo",
                     "bind_value": "super_area_elev_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -755,6 +765,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "Automatic",
                     "bind_mode": "super_area_plain_combo",
                     "bind_value": "super_area_plain_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -766,6 +777,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "Automatic",
                     "bind_mode": "exposed_frontal_area_combo",
                     "bind_value": "exposed_frontal_area_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -777,6 +789,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "As per Code",
                     "bind_mode": "wind_ecc_deck_combo",
                     "bind_value": "wind_ecc_deck_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -788,6 +801,7 @@ WIND_LOAD_TAB_SCHEMA = {
                     "default_mode": "As per Code",
                     "bind_mode": "wind_ll_ecc_combo",
                     "bind_value": "wind_ll_ecc_value",
+                    "default_value": "",
                     "placeholder": "Custom Value",
                     "on_mode_change": "_toggle_wind_custom_input",
                 },
@@ -855,6 +869,7 @@ TEMPERATURE_LOAD_TAB_SCHEMA = {
     "id": "temperature_load_tab",
     "label_width": 240,
     "field_width": 140,
+    "description": {"title": "Description Box", "text": ""},
     "sections": [
         {
             "id": "temperature_inputs_section",
@@ -946,6 +961,7 @@ CUSTOM_LOAD_TAB_SCHEMA = {
     "id": "custom_load_tab",
     "label_width": 260,
     "field_width": 140,
+    "description": {"title": "Description Box", "text": ""},
     "load_case_choices": [
         "DL", "DW", "SIDL", "LL", "EL", "WL", "TL", "Custom"
     ],
@@ -1020,17 +1036,229 @@ CUSTOM_LOAD_TAB_SCHEMA = {
             "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
         },
     },
+    "sections": [
+        {
+            "id": "custom_load_diagram",
+            "type": "diagram",
+            "text": "Bridge Geometry\nDiagram",
+            "min_size": [380, 130],
+            "max_height": 130,
+            "bind": "custom_load_diagram_placeholder",
+        },
+        {
+            "id": "custom_load_case_section",
+            "title": "Custom Load Input Add/Edit:",
+            "fields": [
+                {
+                    "id": "custom_load_case",
+                    "label": "Load Case",
+                    "type": "combo",
+                    "choices": ["DL", "DW", "SIDL", "LL", "EL", "WL", "TL", "Custom"],
+                    "default": "DL",
+                    "bind": "custom_load_case_combo",
+                    "on_change": "_on_load_case_changed",
+                },
+                {
+                    "id": "custom_load_case_name",
+                    "label": "Custom Load Name",
+                    "type": "line",
+                    "placeholder": "Custom",
+                    "bind": "custom_load_case_name_input",
+                    "enabled": False,
+                },
+                {
+                    "id": "custom_load_type",
+                    "label": "Load Type",
+                    "type": "combo",
+                    "choices": ["Point", "Line", "Area"],
+                    "default": "Point",
+                    "bind": "custom_load_type_combo",
+                    "on_change": "_on_custom_load_type_changed",
+                },
+            ],
+        },
+        {
+            "id": "custom_load_stack_section",
+            "type": "stacked",
+            "bind": "custom_load_stack",
+            "switch_source": "custom_load_type_combo",
+            "pages": [
+                {
+                    "match": "Point",
+                    "sections": [
+                        {
+                            "id": "custom_point_left",
+                            "label": "Distance from Left Edge of Bridge (m)",
+                            "type": "line",
+                            "bind": "custom_point_left_input",
+                            "validator": {"type": "double_range", "bottom": 0.0, "top": 1000.0, "decimals": 3},
+                        },
+                        {
+                            "id": "custom_point_bearing",
+                            "label": "Distance from Center Line of Bearing (m)",
+                            "type": "line",
+                            "bind": "custom_point_bearing_input",
+                            "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
+                        },
+                    ],
+                },
+                {
+                    "match": "Line",
+                    "sections": [
+                        {
+                            "id": "custom_line_left_range",
+                            "title": "Distance from Left Edge of Bridge (m):",
+                            "fields": [
+                                {
+                                    "row_fields": [
+                                        {"label": "Start", "type": "label"},
+                                        {
+                                            "id": "custom_line_left_start",
+                                            "type": "line",
+                                            "bind": "custom_line_left_start",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": 0.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                        {"label": "End", "type": "label"},
+                                        {
+                                            "id": "custom_line_left_end",
+                                            "type": "line",
+                                            "bind": "custom_line_left_end",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": 0.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                    ]
+                                }
+                            ],
+                        },
+                        {
+                            "id": "custom_line_bearing_range",
+                            "title": "Distance from Center Line of Bearing (m):",
+                            "fields": [
+                                {
+                                    "row_fields": [
+                                        {"label": "Start", "type": "label"},
+                                        {
+                                            "id": "custom_line_bearing_start",
+                                            "type": "line",
+                                            "bind": "custom_line_bearing_start",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                        {"label": "End", "type": "label"},
+                                        {
+                                            "id": "custom_line_bearing_end",
+                                            "type": "line",
+                                            "bind": "custom_line_bearing_end",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                    ]
+                                }
+                            ],
+                        },
+                    ],
+                },
+                {
+                    "match": "Area",
+                    "sections": [
+                        {
+                            "id": "custom_area_left_range",
+                            "title": "Distance from Left Edge of Bridge (m):",
+                            "fields": [
+                                {
+                                    "row_fields": [
+                                        {"label": "Start", "type": "label"},
+                                        {
+                                            "id": "custom_area_left_start",
+                                            "type": "line",
+                                            "bind": "custom_line_left_start",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": 0.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                        {"label": "End", "type": "label"},
+                                        {
+                                            "id": "custom_area_left_end",
+                                            "type": "line",
+                                            "bind": "custom_line_left_end",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": 0.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                    ]
+                                }
+                            ],
+                        },
+                        {
+                            "id": "custom_area_bearing_range",
+                            "title": "Distance from Center Line of Bearing (m):",
+                            "fields": [
+                                {
+                                    "row_fields": [
+                                        {"label": "Start", "type": "label"},
+                                        {
+                                            "id": "custom_area_bearing_start",
+                                            "type": "line",
+                                            "bind": "custom_line_bearing_start",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                        {"label": "End", "type": "label"},
+                                        {
+                                            "id": "custom_area_bearing_end",
+                                            "type": "line",
+                                            "bind": "custom_line_bearing_end",
+                                            "width": 70,
+                                            "validator": {"type": "double_range", "bottom": -1000.0, "top": 1000.0, "decimals": 3},
+                                        },
+                                    ]
+                                }
+                            ],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            "id": "custom_load_save_section",
+            "fields": [
+                {
+                    "id": "custom_load_save",
+                    "label": "",
+                    "type": "button",
+                    "text": "Save",
+                    "bind": "custom_save_btn",
+                    "on_click": "_on_save_custom_load",
+                    "width": 120,
+                },
+            ],
+        },
+        {
+            "id": "custom_load_table",
+            "title": "Custom Load Name",
+            "type": "custom_load_combo_table",
+            "bind": "custom_load_table",
+            "add_button_bind": "custom_load_table_add_btn",
+            "edit_button_bind": "custom_edit_btn",
+            "delete_button_bind": "custom_delete_btn",
+            "title_bind": "custom_load_table_title",
+            "columns": ["Load Case", "Load Type", "Distance 1", "Distance 2"],
+            "min_table_height": 180,
+        },
+    ],
 }
 
 LOAD_COMBINATION_TAB_SCHEMA = {
     "id": "load_combination_tab",
     "label_width": 280,
+    "description": {"title": "Description Box", "text": ""},
     "sections": [
         {
             "id": "irc_load_combos_section",
             "title": "Load Combinations from IRC 6",
             "type": "dynamic_checkbox_list",
             "bind": "irc_load_combos_checkboxes",
+            "layout_bind": "irc_content_layout",
+            "title_bind": "irc_load_combos_title",
             "default_checked": False,
         },
         {
@@ -1038,7 +1266,15 @@ LOAD_COMBINATION_TAB_SCHEMA = {
             "title": "Custom Load Combination",
             "type": "custom_load_combo_table",
             "bind": "custom_load_combo_table",
+            "columns": ["S.No.", "Combination Name", "Include"],
             "add_button_bind": "load_combo_add_btn",
+            "add_button_text": "Add Custom Combination",
+            "edit_button_bind": "load_combo_edit_btn",
+            "edit_button_text": "Modify",
+            "delete_button_bind": "load_combo_delete_btn",
+            "delete_button_text": "Delete",
+            "title_bind": "custom_combo_title",
+            "min_table_height": 180,
         },
     ],
 }
@@ -1089,6 +1325,40 @@ SUPPORT_CONDITIONS_SCHEMA = {
                 }
             ],
         },
+        {
+            "id": "support_cad_row",
+            "type": "cad_row",
+            "cads": [
+                {
+                    "id": "left_support_cad",
+                    "type": "cad",
+                    "widget": "SupportCADWidget",
+                    "bind": "left_cad",
+                    "min_size": [300, 250],
+                    "size_policy": "expanding",
+                    "stretch": 1,
+                },
+                {
+                    "id": "support_detail_cad",
+                    "type": "cad",
+                    "widget": "SupportDetailCADWidget",
+                    "bind": "right_cad",
+                    "min_size": [150, 200],
+                    "size_policy": "expanding",
+                    "stretch": 1,
+                    "params_map": {
+                        "bearing_length": {
+                            "widget": "bearing_length_input",
+                            "cast": "float",
+                            "default": 400,
+                        },
+                    },
+                    "reactive_sources": [
+                        {"widget": "bearing_length_input", "signal": "textChanged"},
+                    ],
+                },
+            ],
+        },
     ],
 }
 
@@ -1128,7 +1398,8 @@ DESIGN_OPTIONS_SCHEMA = {
                             "label": "Reinforcement Size",
                             "type": "button",
                             "text": "Set Bounds",
-                            "bind": "reinforcement_bounds_btn"
+                            "bind": "reinforcement_bounds_btn",
+                            "on_click": "_open_reinforcement_bounds",
                         },
                         {
                             "id": "reinforcement_material",
