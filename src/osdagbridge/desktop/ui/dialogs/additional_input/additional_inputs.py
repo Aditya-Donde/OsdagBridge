9797e7e186c36aad88a4ce4c962c7409641176f4
mhsuhail00 <mhsuhail00@gmail.com>
2026-07-30 16:13:39 +0530

    fix computed & refresh and comment utilization ratio

--- src/osdagbridge/desktop/ui/dialogs/additional_input/additional_inputs.py
+++ src/osdagbridge/desktop/ui/dialogs/additional_input/additional_inputs.py
@@ -2992,9 +2992,11 @@ class AdditionalInputs(QDialog):
         height_str = working_input_dict.get(KEY_WL_AVG_EXPOSED_HEIGHT)
         terrain_str = working_input_dict.get(KEY_WL_TERRAIN_TYPE)
 
+        if not basic_wind_speed_str:
+            basic_wind_speed_str = working_input_dict.get("project.location", {}).get("weather_data", {}).get("wind_speed", "")
+        
         if not basic_wind_speed_str or not height_str or not terrain_str:
             return {}
-
         try:
             height = float(height_str)
             basic_wind_speed = float(basic_wind_speed_str)
@@ -3018,6 +3020,12 @@ class AdditionalInputs(QDialog):
 
         max_str = working_input_dict.get(KEY_TL_HIGHEST_MAX_TEMP)
         min_str = working_input_dict.get(KEY_TL_LOWEST_MIN_TEMP)
+
+        if not max_str:
+            max_str = working_input_dict.get("project.location", {}).get("weather_data", {}).get("max_temp", "")
+        if not min_str:
+            min_str = working_input_dict.get("project.location", {}).get("weather_data", {}).get("min_temp", "")
+
         if not max_str or not min_str or max_str == "—" or min_str == "—":
             return {}
 
