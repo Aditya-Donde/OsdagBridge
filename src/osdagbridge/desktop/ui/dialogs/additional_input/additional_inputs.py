3aefe5b0b46f57f7e01530a58188f3e5c6313f39
zehen-249 <1hasanmehendi123@gmail.com>
2026-08-01 14:08:28 +0530

    Fix Issue #327, 0 min temp causes no temp value available

--- src/osdagbridge/desktop/ui/dialogs/additional_input/additional_inputs.py
+++ src/osdagbridge/desktop/ui/dialogs/additional_input/additional_inputs.py
@@ -3019,7 +3019,14 @@ class AdditionalInputs(QDialog):
 
         max_str = working_input_dict.get(KEY_TL_HIGHEST_MAX_TEMP)
         min_str = working_input_dict.get(KEY_TL_LOWEST_MIN_TEMP)
-        if not max_str or not min_str or max_str == "—" or min_str == "—":
+        if (
+            max_str is None
+            or min_str is None
+            or max_str == "—"
+            or min_str == "—"
+        ):
+            print("Value None")
+            print(f"Max: {max_str}, Min: {min_str}")
             return {}
 
         try:
